33858e385e0396f58947ff13761fbd33e80a961d
MTtankkeo <ttankkeo112@gmail.com>
2024-02-23 13:32:29 +0900

    small fix

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_touch_ripple/components/behavior.dart';
